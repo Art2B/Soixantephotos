b7ced401df4b840e168d6fbba33812ca46e5ad1b
Arthur Battut <arthur@soixantecircuits.fr>
2015-05-07 17:40:08 +0200

    reorganise routes

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var multer  = require('multer');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var photos = require('./routes/photos');
 
 var db = require('./database/index.js');
 
@@ -30,6 +31,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/photos', photos);
 app.use('/', routes);
 app.use('/users', users);
 
---
+++ routes/photos.js
@@ -0,0 +1,136 @@
+var express = require('express');
+var fs = require('fs');
+var mongoose = require('mongoose');
+var imagesSchema = require('../database/image');
+var _ = require('lodash');
+var multer  = require('multer');
+var uuid = require('uuid');
+var im = require('imagemagick');
+
+var Image = mongoose.model('Image', imagesSchema);
+var router = express.Router();
+
+/* GET ROUTES */
+router.get('/', function(req, res) {
+  res.render('photos/index');
+});
+
+router.get('/:id', function(req, res){
+  Image.findOne({_id: req.params.id}, function(err, photo){
+    if(err){
+      res.status(404).send('Photo not found');
+    } else {
+      res.contentType(photo.img.contentType);
+      res.status(200).send(photo.img.data);
+    }
+  });
+});
+
+/* POST ROUTES */
+router.post('/new', multer({
+  dest: './uploads/',
+  changeDest: function(dest, req, res) {
+      var newDestination = dest + 'images/';
+      var stat = null;
+      try {
+          stat = fs.statSync(newDestination);
+      } catch (err) {
+          fs.mkdirSync(newDestination);
+      }
+      if (stat && !stat.isDirectory()) {
+          throw new Error('Directory cannot be created because an inode of a different type exists at "' + dest + '"');
+      }
+      return newDestination;
+  },
+  rename: function (fieldname, filename, req, res) {
+    return filename.replace(/\W+/g, '-').toLowerCase() + uuid.v4();
+  }
+}),  function(req, res, next){
+
+  im.identify(globals.directory +'/'+ req.files.image.path, function(err, features){
+    if (err) {
+      res.status(500).send('Something goes wrong: ',err);
+    }
+
+    if(features.height > 1920 || features.widht > 1920){
+      var size = {};
+      if(features.height > features.width){
+        size.height = 1920;
+        size.width = 1920/(features.height/features.width);
+      } else {
+        size.height = 1920/(features.height/features.width);
+        size.widht = 1920;
+      }
+      im.resize({
+        srcPath: globals.directory +'/'+ req.files.image.path,
+        dstPath: globals.directory +'/'+ req.files.image.path,
+        width: size.width,
+        height: size.height,
+        quality: 1,
+      }, function(err){
+        if(err){
+          throw err;
+        }
+        createImage(req, features).save(function(err, data){
+          if(err){
+            res.status(500).send('Something goes wrong: ',err);
+            return console.error(err);
+          }
+          fs.unlink(globals.directory +'/'+ req.files.image.path);
+
+          res.status(201).send('Image saved');
+        });
+      });
+    } else {
+      createImage(req, features).save(function(err, data){
+        if(err){
+          res.status(500).send('Something goes wrong: ',err);
+          return console.error(err);
+        }
+        fs.unlink(globals.directory +'/'+ req.files.image.path);
+
+        res.status(201).send('Image saved');
+      });
+    }
+  });
+
+});
+
+/* DELETE ROUTES */
+router.delete('/clear', function(req, res){
+  Image.remove({}, function(err){
+    if(err){
+      res.status(500).send('Something goes wrong: ',err);
+      return console.error(err);
+    }
+    res.status(200).send('Delete all database');
+  })
+});
+router.delete('/clear/:category', function(req, res){
+  Image.remove({category: req.params.category}, function(err){
+    if(err){
+      res.status(500).send('Something goes wrong: ',err);
+      return console.error(err);
+    }
+    res.status(200).send('Delete all category\'s images');
+  })
+});
+
+
+function createImage(req, features){
+  return new Image({
+    name: req.files.image.originalname,
+    category: req.body.category,
+    sfw: req.body.sfw,
+    img: {
+      data: fs.readFileSync(globals.directory +'/'+ req.files.image.path),
+      contentType: req.files.image.mimetype,
+      size: {
+        height: features.height,
+        width: features.width
+      }
+    }
+  });
+}
+
+module.exports = router;
