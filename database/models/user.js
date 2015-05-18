567e62b6d3a37625c0cd040bbd2e6b4bc28d5c61
Arthur Battut <arthur@soixantecircuits.fr>
2015-05-18 23:29:22 +0200

    add user model

---
+++ database/models/user.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var bcrypt = requite('bcrypt');
+
+var userSchema = mongoose.Schema({
+  email: String,
+  password: String,
+  rank: String
+});
+
+// methods ======================
+// generating a hash
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
